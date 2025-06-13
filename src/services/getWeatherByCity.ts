30a0413ae4e8c1383cd57cbec242b7ef8e3215d6
Ana Claudia <anacnogueira@gmail.com>
2025-06-13 16:33:53 -0300

    Explicit typing

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -40,6 +40,19 @@ export interface WeatherResponseProps {
   details: (typeof weatherIcons)["Clear"];
 }
 
+export interface WeatherDetailsResponseProps {
+  feels_like: number;
+  probability: number;
+  wind_speed: number;
+  humidity: number;
+  temp_kf: number;
+}
+
+interface TodayProps {
+  weather: WeatherResponseProps;
+  details: WeatherDetailsResponseProps;
+}
+
 export async function getWeatherByCity({
   latitude,
   longitude,
@@ -47,7 +60,7 @@ export async function getWeatherByCity({
   const { data } = await api.get(`/forecast?lat=${latitude}&lon=${longitude}`);
   const { main, weather, wind, pop } = data.list[0];
 
-  const today = {
+  const today: TodayProps = {
     weather: {
       temp: Math.ceil(main.temp),
       temp_min: Math.floor(main.temp_min),
