9348632438e6069f152a66597ece5264311fc90f
Ana Claudia <anacnogueira@gmail.com>
2025-06-13 16:57:48 -0300

    Finishing today component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,20 @@
-import './styles.css';
+import "./styles.css";
 
-import { useEffect, useState } from 'react';
-import { getWeatherByCity } from '../../services/getWeatherByCity';
+import { useEffect, useState } from "react";
+import { getWeatherByCity } from "../../services/getWeatherByCity";
 
-import { Today } from '../../components/Today';
-import { Details } from '../../components/Details';
-import { Loading } from '../../components/Loading';
-import { NextDays } from '../../components/NextDays';
+import { Today } from "../../components/Today";
+import { Details } from "../../components/Details";
+import { Loading } from "../../components/Loading";
+import { NextDays } from "../../components/NextDays";
+import { CityProps } from "../../services/getCityByNameService";
 
 export function Dashboard() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const [city, setCity] = useState(JSON.parse(localStorage.getItem('@typewheather:city')));
+  const [city, setCity] = useState<CityProps>(
+    JSON.parse(localStorage.getItem("@typewheather:city") ?? "")
+  );
 
   useEffect(() => {
     setIsLoading(true);
@@ -24,14 +27,18 @@ export function Dashboard() {
   }, [city]);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
-    <div className='dashboard'>
-      <Today city={city.name} onSearchValue={setCity} weather={data.today.weather} />
+    <div className="dashboard">
+      <Today
+        city={city.name}
+        onSearchValue={setCity}
+        weather={data.today.weather}
+      />
       <Details data={data.today.details} />
       <NextDays data={data.nextDays} />
     </div>
-  )
+  );
 }
